e522866532521ff33c3322b2f08acec20240b9b6
sharathr25 <sharathdupati@gmail.com>
2021-12-25 12:22:11 +0530

    changed dev port

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :ludo_server, LudoServer.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :ludo_server, LudoServerWeb.Endpoint,
-  http: [port: 5000],
+  http: [port: 3333],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
